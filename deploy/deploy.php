41ef44c404c6bf95ea4cabc0c30f15b33286bc2e
Roberto Rielo <roberto910907@gmail.com>
2019-03-10 00:20:54 -0500

    Added new commands for deployment

--- deploy/deploy.php
+++ deploy/deploy.php
@@ -2,17 +2,27 @@
 
 namespace Deployer;
 
-require 'recipe/symfony.php';
+require 'recipe/symfony4.php';
 
 inventory('hosts.yml');
 
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true);
 
+set('shared_dirs', ['var/log', 'var/sessions', 'vendor']);
+
 set('release_name', function () {
     return date('YmdHis');
 });
 
-// Tasks
+set('env', [
+    'APP_ENV' => 'production',
+]);
+
+desc('Update database schema');
+task('deploy:schema:update', '{{bin/console}} doctrine:schema:update --force');
+
+before('success', 'deploy:schema:update');
+after('deploy:failed', 'deploy:unlock');
 
 
