4aed5661aa2be9b50b0d4e716f66657652bf525b
Ubel <ubelangelfonseca@gmail.com>
2019-03-20 17:49:24 -0600

    remove bug to remove user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -113,7 +113,7 @@ class User extends BaseUser
     private $updatedAt;
 
     /**
-     * @ORM\OneToOne(targetEntity="App\Entity\Resume", mappedBy="user",cascade={"persist"} )
+     * @ORM\OneToOne(targetEntity="App\Entity\Resume", mappedBy="user",cascade={"persist", "remove"} )
      */
     private $resume;
 
