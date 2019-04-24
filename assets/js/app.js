5dfafb8527671e19a7fb38f8e78cdedf88013040
Ubel <ubelangelfonseca@gmail.com>
2019-04-24 16:52:35 -0500

    webpack

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you require will output into a single css file (app.css in this case)
+require('../css/app.css');
+require('../../public/site/assets/css/bootstrap.min.css');
+require('../../public/site/assets/css/fontawesome-all.min.css');
+require('../../public/site/assets/css/themify-icons.css');
+require('../../public/site/assets/css/et-line.css');
+require('../../public/site/assets/css/bootstrap-select.min.css');
+require('../../public/site/assets/css/flag.css');
+require('../../public/site/assets/css/slick.css');
+require('../../public/site/assets/css/owl.carousel.min.css');
+require('../../public/site/assets/css/jquery.nstSlider.min.css');
+require('../../public/site/css/main.css');
+require('../../public/site/css/bootstrap-multiselect.css');
+
+// const $ = require('jquery');
+// require('../../public/site/assets/js/jquery.min.js');
+// require('../../public/site/assets/js/bootstrap-select.min.js');
+// require('../../public/site/js/ajax.js');
+// require('../../public/site/assets/js/feather.min.js');
+// require('../../public/site/assets/js/jquery.nstSlider.min.js');
+// require('../../public/site/assets/js/visible.js');
+// require('../../public/site/assets/js/jquery.countTo.js');
+// require('../../public/site/assets/js/chart.js');
+// require('../../public/site/assets/js/plyr.js');
+// require('../../public/site/assets/js/tinymce.min.js');
+// require('../../public/site/assets/js/slick.min.js');
+// require('../../public/site/assets/js/jquery.ajaxchimp.min.js');
+// require('../../public/site/js/custom.js');
+// require('../../public/site/js/map.js');
+// require('../../public/site/assets/js/owl.carousel.min.js');
+// require('../../public/site/js/searchMap.js');
+// require('../../public/site/js/validate.js');
+// require('../../public/site/js/utils.js');
+
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
