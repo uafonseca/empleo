cfdc51ec44015454116f92f76e44f195236035d1
ubel <ubelangelfonseca@gmail.com>
2020-06-25 16:17:43 -0400

    resume edit

--- webpack.config.js
+++ webpack.config.js
@@ -75,7 +75,7 @@ Encore
     // .enableSassLoader()
 
     // uncomment if you use TypeScript
-    .enableTypeScriptLoader()
+    // .enableTypeScriptLoader()
 
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
