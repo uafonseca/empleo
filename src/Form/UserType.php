46ce236d3951511000fb2b3780be1a1bc93154d3
Ubel <ubelangelfonseca@gmail.com>
2021-11-17 11:38:55 -0500

    updates

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -15,6 +15,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
@@ -31,42 +33,41 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class , [ //
-            'label' => false,
-            'attr' => ['placeholder' => 'Nombre(s)']
-        ])
-            ->add('email', EmailType::class , [ //
-            'label' => false,
-            'attr' => ['placeholder' => 'Correo electrónico']
-        ])
+            ->add('name', TextType::class, [ //
+                'label' => false,
+                'attr' => ['placeholder' => 'Nombre(s)']
+            ])
+            ->add('email', EmailType::class, [ //
+                'label' => false,
+                'attr' => ['placeholder' => 'Correo electrónico']
+            ])
             ->add('username', null, [ //
-            'label' => false,
-            'attr' => ['placeholder' => 'Nombre de usuario']
-        ])
-            ->add('employer', ChoiceType::class , [
-            'choices' => array(
-                'Registrarme como empleador' => true,
-                'Registrarme como usuario' => false,
-            ),
-            'expanded' => true,
-            'label' => false,
-            'required' => true,
-            'data' => false
-        ])
-            ->add('phone', null, [ //
-            'label' => false,
-            'attr' => ['placeholder' => 'Teléfono'],
-            'constraints' => [new Regex('/[^0-9]/')],
-        ])
-            ->add('plainPassword', RepeatedType::class , array(
-            'type' => PasswordType::class ,
-            'invalid_message' => 'Las contraseñas deben ser iguales',
-            'first_options' => ['attr' => ['placeholder' => 'Contraseña'], 'label' => false],
-            'second_options' => ['attr' => ['placeholder' => 'Repita su contraseña', 'label' => false]],
-            'label' => false,
-            'constraints' => [new Length(['min' => 6])],
-        ))
-            ;
+                'label' => false,
+                'attr' => ['placeholder' => 'Nombre de usuario']
+            ])
+            ->add('employer', ChoiceType::class, [
+                'choices' => array(
+                    'Registrarme como empleador' => true,
+                    'Registrarme como usuario' => false,
+                ),
+                'expanded' => true,
+                'label' => false,
+                'required' => true,
+                'data' => false
+            ])
+            ->add('phone', IntegerType::class, [ //
+                'label' => false,
+                'attr' => ['placeholder' => 'Teléfono'],
+                'constraints' => [new Regex('/[^0-9]/')],
+            ])
+            ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'invalid_message' => 'Las contraseñas deben ser iguales',
+                'first_options' => ['attr' => ['placeholder' => 'Contraseña'], 'label' => false],
+                'second_options' => ['attr' => ['placeholder' => 'Repita su contraseña', 'label' => false]],
+                'label' => false,
+                'constraints' => [new Length(['min' => 6])],
+            ));
     }
 
     public function buildFormFully(FormBuilderInterface $builder, array $options)
@@ -77,7 +78,7 @@ class UserType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => User::class ,
+            'data_class' => User::class,
         ]);
     }
 }
